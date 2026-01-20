9e5c238890e1bd6d4e6fc55fd552920709b11d9a
画上佳人纱 <2841541624@qq.com>
2026-01-20 17:49:10 +0800

    update(stores): 补充导出

--- packages/@buildingai/web/stores/src/index.ts
+++ packages/@buildingai/web/stores/src/index.ts
@@ -1,3 +1,5 @@
 export { createStore } from "./create-store";
 export * from "./slices/auth.slice";
 export * from "./slices/config.slice";
+export { getLocalStorage, safeJsonParse, safeJsonStringify } from "./utils/storage";
+export type { StorageAdapter } from "./utils/storage";
